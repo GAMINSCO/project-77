cc96b2626b6c30e4fffc3cf4a925485a121a422f
GAMINSCO <83284620+GAMINSCO@users.noreply.github.com>
2021-08-06 12:25:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+//base var
+var f_name = "Daniel";
+var l_name = " W. S";
+var a = 5;
+var s = 10;
+
+//console
+console.log(f_name + l_name + " nickname: DaniDev");
+console.log(a * 5 - s + 20 - s * 2);
